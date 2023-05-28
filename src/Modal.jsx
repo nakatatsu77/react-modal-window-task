0cc8a0ec7a771513d71c0f52de1facc9ab2679e6
nakatatsu77 <nakatatsu1177@gmail.com>
2023-05-28 23:23:13 +0900

    背景クリックでモーダルを閉じる機能を追加

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,5 +1,5 @@
-import "./Modal.css";
 import React, { useState } from "react";
+import "./Modal.css";
 
 const Modal = () => {
   // モーダルの表示、非表示の状態管理。初期はfalseにする
@@ -11,14 +11,24 @@ const Modal = () => {
     setIsOpen(!isOpen);
   };
 
+  // 背景クリックでモーダルを閉じる
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  // モーダルの内部クリックでもモーダルが閉じるの防ぐ処理
+  const handleModalContentClick = (e) => {
+    e.stopPropagation();
+  };
+
   return (
     <div>
       <button onClick={toggleModal}>モーダルを開く</button>
 
       {/* isOpenがtrueの場合、モーダルを表示する */}
       {isOpen && (
-        <div className="modal">
-          <div className="modalContent">
+        <div className="modal" onClick={closeModal}>
+          <div className="modalContent" onClick={handleModalContentClick}>
             <h2>モーダルウィンドウ</h2>
             <span onClick={toggleModal} className="modalClose">
               &times;
